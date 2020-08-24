04b822ab283d4ccf696fff3cb26146aa2627fe1b
david35008 <67857450+david35008@users.noreply.github.com>
2020-08-24 13:18:10 +0300

    install ESLINT

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import Ticket from './components/Ticket'
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <main>
+      Hello World
+
     </main>
   );
 }
---
+++ server/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+  },
+};
