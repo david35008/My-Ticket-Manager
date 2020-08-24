ca3950a16b6d1bff83a77f817a9a65da32cc5d72
david35008 <67857450+david35008@users.noreply.github.com>
2020-08-24 15:50:51 +0300

    add get  API endpoint

--- server/app.js
+++ server/app.js
@@ -3,37 +3,41 @@ const fs = require('fs').promises;
 
 const app = express();
 
-// const path =  process.env.GLOBAL_VAR || './records.json';
-// const path = process.env.NODE_ENV === 'test' ? './recordsTest.json' : './records.json';
-// console.log(process.env.NODE_ENV);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', express.static('../client/build'));
+// app.use('/', express.static('../client/build'));
 
-app.get('/api/records', async (req, res) => {
-  const content = await fs.readFile(path);
+// JSON.stringify(allTickets, null, 2)
+
+app.get('/api/tickets', async (req, res) => {
+  const content = await fs.readFile('./data.json');
   const json = JSON.parse(content);
-  res.send(json);
+  if (req.query.searchText) {
+    const filteredTickets = json
+      .filter((ticket) => ticket.title.toLowerCase().includes(req.query.searchText));
+    res.send(filteredTickets);
+  } else {
+    res.send(json);
+  }
 });
 
-app.post('/api/records', async (req, res) => {
-  const content = await fs.readFile(path);
-  const json = JSON.parse(content);
-  json.push(req.body);
-  const message = JSON.stringify(json);
-  await fs.writeFile(path, message);
-  res.send(json);
-});
+// app.post('/api/records', async (req, res) => {
+//   const content = await fs.readFile(path);
+//   const json = JSON.parse(content);
+//   json.push(req.body);
+//   const message = JSON.stringify(json);
+//   await fs.writeFile(path, message);
+//   res.send(json);
+// });
 
-app.delete('/api/records', async (req, res) => {
-  await fs.writeFile(path, '[{"id":"1","winnerName":"no body","date":"never","gameDuration":"0"}]');
-  res.send('deleteed');
-});
+// app.delete('/api/records', async (req, res) => {
+// await fs.writeFile(path, '[{"id":"1","winnerName":"no body","date":"never","gameDuration":"0"}]');
+//   res.send('deleteed');
+// });
 
-app.get('/', (req, res) => {
-  res.send('hello');
-});
+// app.get('/', (req, res) => {
+//   res.send('hello');
+// });
 
 module.exports = app;
